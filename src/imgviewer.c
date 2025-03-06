72b1dea0fc0857837fe1f6ee5e8ca9a55a864788
AIrvenRichardson <airvenrichardson@aol.com>
2025-03-05 23:20:15 -0700

    Added the ability to open gifs, and made the window open at image size or screen size by default

--- src/imgviewer.c
+++ src/imgviewer.c
@@ -1,43 +1,68 @@
 #include "include/raylib.h"
-
 #define RAYGUI_IMPLEMENTATION
 #include "include/raygui.h"
 
+int minimum(int a, int b)
+{
+    return (a < b) ? a : b;
+}
+
 int main(int argc, char *argv[])
 {
+    //Configure the window
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     int screenWidth = 1000;
     int screenHeight = 500;
     InitWindow(screenWidth, screenHeight, "I HATE WINDOWS!!!!");
     SetTargetFPS(60);
 
+    //Make room for an image
     Image image = { 0 };
 
-    if (argc > 1)
-    {
-        image = LoadImage(argv[1]);
-    }
-    
+    if (argc > 1) image = LoadImage(argv[1]); 
 
+    //A ton of variables for the image
     Texture2D drawTarget = { 0 };
-    bool newImage = true;
     float imageScale = 1.0f;
     float xoffset = 0.0f;
     float yoffset = 0.0f;
 
+    //Animation Controls
+    unsigned int nextFrameDataOffset = 0;
+    int animFrames = 0;
+    int currentAnimFrame = 0;
+    int frameDelay = 3;
+    int frameCounter = 0;
+    
+    if (image.format == 7) image = LoadImageAnim(argv[1], &animFrames); //Reload image as an animation if it's a gif (7)
+
+    if (IsImageValid(image))
+    {
+        drawTarget = LoadTextureFromImage(image);
+        SetWindowSize(minimum(image.width, GetMonitorWidth(0)), minimum(image.height,GetMonitorHeight(0)));
+    }
+
     while (!WindowShouldClose())
     {
+        //UPDATE
+        if (image.format == 7)
+        {
+            frameCounter++;
+            if (frameCounter >= frameDelay)
+            {
+                currentAnimFrame++;
+                if (currentAnimFrame >= animFrames) currentAnimFrame = 0;
+                nextFrameDataOffset = image.width*image.height*4*currentAnimFrame;
+                UpdateTexture(drawTarget, ((unsigned char *)image.data) + nextFrameDataOffset);
+                frameCounter = 0;
+            }
+        }
+
         if (IsWindowResized() && !IsWindowFullscreen())
         {
             screenWidth = GetScreenWidth();
             screenHeight = GetScreenHeight();
         } 
-
-        if (IsImageValid(image) && newImage)
-        {
-            drawTarget = LoadTextureFromImage(image);
-            newImage = false;    
-        }
 
         imageScale += (float)GetMouseWheelMove()*0.05f;   // Image scale control
         if (imageScale <= 0.1f) imageScale = 0.1f;
